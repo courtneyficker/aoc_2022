8b464be3b228c8b4a6e942ab8c749c28be693bf2
Courtney Ficker <courtney.ficker@gmail.com>
2022-12-20 23:55:53 -0800

    Finished Day 7 part 1

---
+++ 7/1.py
@@ -0,0 +1,126 @@
+# Advent of Code 2022 - Day 7 Part 1
+# https://adventofcode.com/2022/day/6
+# Courtney Ficker
+
+# File system
+class FileSystem:
+    def __init__(self) -> None:
+        self.root = Directory('/')
+        self.currentDir = self.root
+
+    def __sizeof__(self) -> int:
+        return self.root.__sizeof__()
+
+    def __str__(self) -> str:
+        return str(self.root.__str__())
+
+    def cd(self, dirName: str):
+        # Go to root
+        if (dirName == '/'):
+            # print('changing current directory to ROOT')
+            self.currentDir = self.root
+            return
+        # Go up one level if not already at root
+        if (dirName == '..'):
+            # print('changing current directory to UP ONE LEVEL')
+            if (self.currentDir.name != '/'):
+                self.currentDir = self.currentDir.parent
+            return
+        # Go to specified dir, if it exists in this directory
+        newDir = self.currentDir.getDir(dirName)
+        if (newDir):
+            # print(f'changing current directory to {newDir.name}')
+            self.currentDir = newDir
+        return
+
+
+class Directory:
+    def __init__(self, name):
+        self.name = name
+        self.files = []
+        self.directories = []
+        self.indent = 0
+        self.parent = None
+
+    def __sizeof__(self) -> int:
+        size = 0
+        for file in self.files:
+            size += file.__sizeof__()
+        for dir in self.directories:
+            size += dir.__sizeof__()
+        return size
+
+    def __str__(self) -> str:
+        return f'{len(self.files)} files, {len(self.directories)} directories: {self.__sizeof__()} bytes'
+
+    def addFile(self, f) -> None:
+        self.files.append(f)
+
+    def addDir(self, d) -> None:
+        d.indent = self.indent + 2
+        d.parent = self
+        self.directories.append(d)
+
+    def getDir(self, directoryName):
+        for d in self.directories:
+            if (d.name == directoryName):
+                return d
+
+    def ls(self) -> None:
+        indents = ' ' * self.indent
+        print(f'{indents}- {self.name} (dir)')
+        for dir in self.directories:
+            dir.ls()
+        for f in self.files:
+            print(f'{indents}- {f.name} (file, size={f.size}')
+
+    def calc_answer_a(self, limit) -> int:
+        ret = 0
+        size = self.__sizeof__()
+        if (size <= limit):
+            ret += size
+        for d in self.directories:
+            ret += d.calc_answer()
+        return ret
+
+class File:
+    def __init__(self, name, size) -> None:
+        self.name = name
+        self.size = size
+
+    def __sizeof__(self) -> int:
+        return self.size
+
+    def __str__(self) -> str:
+        return f'{self.name}: {self.size} bytes'
+
+fs = FileSystem()
+
+def ls() -> None:
+    fs.ls()
+
+
+# Build the file system
+with open('input.txt','r') as f:
+    for line in f:
+        sp = line.strip().split()
+        if (sp[0] == '$'):
+            # print(f'command: {sp}')
+            cmd = sp[1]
+            if (cmd == 'cd'):
+                fs.cd(sp[2])
+            elif (cmd == 'ls'):
+                # ls()
+                continue
+        elif (sp[0] == 'dir'):
+            newDir = Directory(sp[1])
+            # print(f'adding new Dir: {newDir}')
+            fs.currentDir.addDir(newDir)
+        elif (sp[0].isdigit):
+            # print(f'creating new file \'{sp[1]}\' with size {sp[0]}')
+            newFile = File(sp[1], int(sp[0]))
+            fs.currentDir.addFile(newFile)
+
+# Find the answer
+print('Combined size of directories <= 100000:', fs.root.calc_answer_a(100000))
+
